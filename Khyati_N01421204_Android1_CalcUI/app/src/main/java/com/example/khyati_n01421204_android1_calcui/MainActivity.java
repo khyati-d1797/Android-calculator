01123700fb29622e583b598533b2b3a130f7437e
khyati-d1797 <99372538+khyati-d1797@users.noreply.github.com>
2022-02-10 17:01:13 -0500

    First build

---
+++ Khyati_N01421204_Android1_CalcUI/app/src/main/java/com/example/khyati_n01421204_android1_calcui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.khyati_n01421204_android1_calcui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
